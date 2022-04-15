e57984a647c35b78b90f677103693e80f3a7348f
asena1006 <danny001006@naver.com>
2022-04-16 02:10:05 +0900

    hsb 1tier complete

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,14 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView
+
 
 def hello_world(request):
     return render(request, 'accountapp/hello_world.html')
+
+
+class AccountCreateView(CreateView):
+    model = User
