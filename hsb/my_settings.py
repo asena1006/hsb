b358aa23858f6db91ffe2d80e1e97e7926f10d4c
asena1006 <danny001006@naver.com>
2022-05-15 11:29:36 +0900

    my_settings added

---
+++ hsb/my_settings.py
@@ -0,0 +1,10 @@
+EMAIL = {
+'EMAIL_BACKEND' : 'django.core.mail.backends.smtp.EmailBackend',
+'EMAIL_USE_TLS' : True,
+'EMAIL_PORT' : 587,
+'EMAIL_HOST' : 'smtp.naver.com',
+'EMAIL_HOST_USER' : 'hskanbu0501',
+
+'DEFAULT_FROM_MAIL' : 'hskanbu0501'
+}
+
