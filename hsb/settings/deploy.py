32778d441ec25bc8a91ffecf31d8eb94debd8d95
asena1006 <danny001006@naver.com>
2022-05-15 20:21:20 +0900

    docer secret

--- hsb/settings/deploy.py
+++ hsb/settings/deploy.py
@@ -2,6 +2,13 @@
 
 from .base import *
 
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -24,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -38,7 +45,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'password1234',
+        'PASSWORD': read_secret('MYSQL_PASSWORD')
         'HOST': 'mariadb',
         'PORT': '3306',
     }
@@ -49,5 +56,5 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.naver.com'
 EMAIL_HOST_USER = 'hskanbu0501'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = read_secret('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_MAIL = 'hskanbu0501'
